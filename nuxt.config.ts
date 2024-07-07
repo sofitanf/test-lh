60972dbbf8c2359323dbc04f7760f59fc0633367
Sofita Nur Fitriana <fitahas@gmail.com>
2024-07-07 23:09:44 +0700

    add: build img folder

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,16 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+  vite: {
+    build: {
+      rollupOptions: {
+        external: (id) => {
+          // Match all files in the 'images' directory
+          return id.startsWith('/images/');
+        },
+      },
+    },
+  },
   app: {
     head: {
       charset: 'utf-8',
